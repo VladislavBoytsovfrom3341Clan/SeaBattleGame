52ba3a74a0aea0da29a1fcbb7f26c3fc8d314121
VladislavBoytsovfrom3341Clan <vladybout@gmail.com>
2024-09-26 18:16:44 +0300

    ShipManager class refactoried

--- SeaBattleGame/include/ShipManager.h
+++ SeaBattleGame/include/ShipManager.h
@@ -1,21 +1,39 @@
 #ifndef SHIP_MANAGER
 #define SHIP_MANAGER
 
-#include<utility>
+#include<utility>          //for std::pair
 #include<initializer_list>
 #include"Battleship.h"
 #include"Battlefield.h"
 
+/**
+ * This class contains all placed-on-field ships
+ * and yet not placed ones, also taking responsibility for
+ * calling fields placeShip methods.
+ * It will be used to count how many ships there are left,
+ * and then to end up the game
+*/
 class ShipManager
 {
+    //not placed on field ships
     std::vector<Battleship> mInactiveShipArray;
+
+    //placed on field ships
     std::vector<Battleship> mActiveShipArray;
 
 public:
+
+    //gets list of pairs <shipLength, amount>, places all to inactive ships
     ShipManager(std::initializer_list<std::pair<int, int>> shipList);
+
     int getAliveShipsNumber();
+
     int getInactiveShipsNumber();
+
+    //returns link to const vector of all inactive ships
     const std::vector<Battleship>& getInactiveShips() const;
+
+    //nethod will be used by player/bot interface to call fields method
     void setShipToBattlefield(Battlefield& field, int shipIndex, int x, int y, Orientation orientation);
 };
 
--- SeaBattleGame/sources/ShipManager.cpp
+++ SeaBattleGame/sources/ShipManager.cpp
@@ -10,10 +10,9 @@ ShipManager::ShipManager(std::initializer_list<std::pair<int, int>> shipList)
             throw std::invalid_argument("Invalid ship length");
         if(shipSeries.second<=0)
             throw std::invalid_argument("Ships number must be greater than zero");
+
         for(int i=0; i<shipSeries.second; i++)
-        {
             mInactiveShipArray.emplace_back(shipSeries.first);
-        }
     }
 }
 
@@ -25,9 +24,13 @@ const std::vector<Battleship>& ShipManager::getInactiveShips() const
 int ShipManager::getAliveShipsNumber()
 {
     int shipNum=0;
+
+    //counts every inactive ship
     for(auto ship: mInactiveShipArray)
         if(ship.isAlive())
             shipNum++;
+
+    //and every active ship
     for(auto ship: mActiveShipArray)
         if(ship.isAlive())
             shipNum++;
@@ -40,8 +43,14 @@ void ShipManager::setShipToBattlefield(Battlefield& field, int shipIndex, int x,
         throw std::logic_error("No ships to be set available");
     if (shipIndex<0 or shipIndex>=mInactiveShipArray.size())
         throw std::invalid_argument("Invalid ship index");
+    
+    //may throw an exception from field, so needs to be processed once being called
     field.setShip(&mInactiveShipArray.at(shipIndex), x, y, orientation);
+
+    //adds ship to an active vector
     mActiveShipArray.push_back(mInactiveShipArray.at(shipIndex));
+
+    //and delete fron inactive
     mInactiveShipArray.erase(mInactiveShipArray.begin() + shipIndex);   
 }
 
